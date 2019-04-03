1329ef1256c672187dd2153f0956a75f4088791c
Tai Dang <tai.dangit@gmail.com>
2019-04-02 23:17:49 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,20 @@
+const greetings = require("./greeting.js");
+const tests = require("./test.js");
+
+// "Hello"
+greetings.greetingEnglish();
+
+// "Konichiwa"
+greetings.greetingJapanese();
+
+// "Hola"
+greetings.greetingSpanish();
+
+// "Hallo"
+greetings.greetingIcelandic();
+
+// "function one"
+tests.one();
+
+// "function two"
+tests.two();
---
+++ test.js
@@ -0,0 +1,7 @@
+exports.one = function(){
+   console.log("function one");
+}
+  
+exports.two = function(){
+   console.log("function two");
+}
